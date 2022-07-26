584f22b2d3e4dbee4f9612c84d4f8781ff40555c
Jordan Asano <jordanasano@yahoo.com>
2022-07-26 15:39:13 -0700

    worked on models

--- models.py
+++ models.py
@@ -1,12 +1,119 @@
 """SQLAlchemy models for ShareB&B."""
 
 from datetime import datetime
+from unicodedata import decimal
 
 from flask_bcrypt import Bcrypt
 from flask_sqlalchemy import SQLAlchemy
 
 bcrypt = Bcrypt()
 db = SQLAlchemy()
+
+class User(db.Model):
+    """User in the system."""
+
+    __tablename__ = 'users'
+
+    id = db.Column(
+        db.Integer,
+        primary_key=True,
+    )
+
+    username = db.Column(
+        db.String(20),
+        nullable=False,
+        unique=True,
+    )
+
+    password = db.Column(
+        db.Text,
+        nullable=False,
+    )
+
+    first_name = db.Column(
+        db.String(40),
+        nullable=False,
+    )
+
+    last_name = db.Column(
+        db.String(40),
+        nullable=False,
+    )
+
+class Message(db.Model):
+    """An individual message."""
+
+    __tablename__ = 'messages'
+
+    id = db.Column(
+        db.Integer,
+        primary_key=True,
+    )
+
+    title = db.Column(
+        db.String(50),
+        nullable=False,
+    )
+
+    text = db.Column(
+        db.Text,
+        nullable=False,
+    )
+
+    timestamp = db.Column(
+        db.DateTime,
+        nullable=False,
+        default=datetime.utcnow,
+    )
+
+    from_user_id = db.Column(
+        db.Integer,
+        db.ForeignKey('users.id', ondelete='CASCADE'),
+        nullable=False,
+    )
+
+    listing_id = db.Column(
+        db.Integer,
+        db.ForeignKey('listings.id', ondelete='CASCADE'),
+        nullable=False,
+    )
+
+
+class Listing(db.Model):
+    """An individual listing."""
+
+    __tablename__ = 'messages'
+
+    id = db.Column(
+        db.Integer,
+        primary_key=True,
+    )
+
+    owner_id = db.Column(
+        db.Integer,
+        db.ForeignKey('owners.id', ondelete='CASCADE'),
+        nullable=False,
+    )
+
+    title = db.Column(
+        db.String(50),
+        nullable=False,
+    )
+
+    price = db.Column(
+        db.Numeric(decimal_return_scale=2),
+        nullable=False,
+    )
+
+    location = db.Column(
+        db.String(50),
+        nullable=False,
+    )
+
+    description = db.Column(
+        db.Text,
+    )
+
 
 
 def connect_db(app):
