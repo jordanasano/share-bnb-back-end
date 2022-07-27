c37b317eb01f5099ac95d698547cc1c293512e70
Jordan Asano <jordanasano@yahoo.com>
2022-07-26 17:21:13 -0700

    finished doc strings for routes

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ def get_user_id():
 def get_listings():
     """Gets all listings.
         Returns JSON: list of 'listing' dicts
-        { owner, title, description, price, location, images: [
+        { id, owner, title, description, price, location, images: [
             { path, description } ]
         }
     """
@@ -51,9 +51,12 @@ def add_listing():
 
         { title, description, price, location, [ { image_file, description }, ... ] }
 
-        => Returns 201 and JSON:
+        => If successful, returns 201 and JSON:
         TODO: what to return to show it was added?
         { id, status: 201, title }
+
+        If error, returns 400 and JSON:
+        { errors, status: 400 }
     """
     #TODO:
     return "listing returned"
@@ -64,10 +67,67 @@ def get_listing(listing_id):
     """Get listing based on id in param.
 
         Returns JSON:
-        { title, description, price, location, [ { path, description }, ... ] }
+        { id, title, description, price, location, [ { path, description }, ... ] }
 
     """
     #TODO:
     return "listing returned"
+
+
+######## MESSAGES #############################################################
+@app.get('/messages')
+def get_messages():
+    """Gets all messages for current user using user id saved in g 
+        Returns JSON: list of 'messages' dicts
+        [ { id, title, text, timestamp, listing_title }, ... ]
+    """
+    #TODO:
+    return "messages returned"
+
+@app.post('/messages')
+def add_message():
+    """Add new message using user id saved in g as from_user_id.
+
+        { title, text, listing_id }
+
+        => If successful, returns 201 and JSON:
+        TODO: what to return to show it was added?
+        { id, status: 201, title }
+
+        If error, returns 400 and JSON:
+        { errors, status: 400 }
+    """
+    #TODO:
+    return "message returned"
+
+
+@app.get('/messages/<int:message_id>')
+def get_message(message_id):
+    """Get message based on id in param.
+
+        Returns JSON:
+        { id, title, text, timestamp, listing_title }
+
+    """
+    #TODO:
+    return "message returned"
+
+######## USER AUTH #############################################################
+@app.post('/login')
+def login():
+    """ Handle user login 
+        Returns JSON: { token }
+    """
+    #TODO:
+    return 'login_token'
+
+@app.post('/signup')
+def signup():
+    """ Handle user signup 
+        Returns JSON: { token }
+    """
+    #TODO:
+    return 'signup_token'
+
 
 connect_db(app)
